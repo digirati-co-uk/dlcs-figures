108dffbdd45e4da6fe403e7e6a9de951b5d25ed8
tomcrane <tom@nanoplanet.com>
2015-04-03 20:54:47 +0100

    improvements to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
-/**
- * Created by tom on 03/04/15.
- */
+
 var gulp = require('gulp');
 var data = require('gulp-data');
 var fm = require('front-matter');
@@ -15,7 +13,7 @@ var fileList = [];
 gulp.task('indexSite', function () {
     console.log("in indexSite task");
     return gulp.src('md-src/**/*.md')
-        // Extract YAML front-matter, convert content to markdown via gulp-data
+        // Extract YAML front-matter, build a list of the md files so we can sort them
         .pipe(data(function (file) {
             var content = fm(String(file.contents));
             var name = file.relative.substr(0, file.relative.length - 3);
@@ -59,9 +57,14 @@ gulp.task('default', ['indexSite'], function () {
     // Compile a template for rendering each page
     var template = hogan.compile(String(fs.readFileSync('template.html')));
 
-    return gulp.src('md-src/**/*.md')
+    // ...and for (one-off) rendering the index page
+    var indexTemplate = hogan.compile(String(fs.readFileSync('index.template.html')));
+
+    var pipeOutput = gulp.src('md-src/**/*.md')
 
         // Extract YAML front-matter, convert content to markdown via gulp-data
+        // gulp-data attaches a "data" property to the piped file
+        // the passed function here is adding various other things as well
         .pipe(data(function (file) {
             var content = fm(String(file.contents));
             //console.log('yaml:',file.relative);
@@ -72,33 +75,56 @@ gulp.task('default', ['indexSite'], function () {
             var name = file.relative.substr(0, file.relative.length - 3);
             file.name = name;
 
-            var list = [];
-
-            var sortedList = sortNav(fileList);
-
-            console.log(sortedList);
-
-            sortedList.forEach(function (item) {
-                var active = false;
-                if (item.name === name) {
-                    active = true;
-                }
-                list.push({name: item.name, title: item.title, active: active});
-            });
-            file.list = list;
-            //console.log(list);
+            // this becomes file.data:
             return content.attributes;
         }))
 
 
         // Run each file through a template
-        .pipe(es.map(function (file, cb) {
-            //console.log('file:',fileList);
+        .pipe(es.map(function (file, callback) {
             file.contents = new Buffer(template.render(file));
-
-            cb(null, file);
+            callback(null, file);
         }))
+
+        // change the file extension
         .pipe(ext_replace('.html'))
+
         // Output to build directory
         .pipe(gulp.dest('./gen/'));
+
+
+    var indexFile = gulp.src("index.html")
+        .pipe(es.map(function (file, callback) {
+            var list = [];
+            var sortedList = sortNav(fileList);
+            sortedList.forEach(function (item) {
+                list.push({name: item.name, title: item.title});
+            });
+            file.list = list;
+            file.contents = new Buffer(indexTemplate.render(file));
+            callback(null, file);
+        }))
+        .pipe(gulp.dest('./'));
+
+
+    return pipeOutput;
+});
+
+
+// http://knsv.github.io/mermaid/mermaidCLI.html
+/*
+/ To run this task, you need mermaid installed globally and PhantomJS on the path
+ */
+gulp.task('makepngs', function () {
+
+    console.log("Generating .PNG files...");
+
+    return gulp.src('md-src/**/*.md')
+
+        // Extract YAML front-matter, build a list of the md files so we can sort them
+        .pipe(data(function (file) {
+            var content = fm(String(file.contents));
+            var name = file.relative.substr(0, file.relative.length - 3);
+            fileList.push({name: name, title: content.attributes.title, order: content.attributes.order});
+        }));
 });
---
+++ md-src/zzz.md
@@ -0,0 +1,11 @@
+---
+title: Tom's first sequence
+order: 1
+---
+
+
+```
+sequenceDiagram
+    Tom->>Adam: Hello Adam, how are you?
+    Adam-->>Tom: Great!
+```
